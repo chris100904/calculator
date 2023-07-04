0c33d1f7531252f7d2731e7dc069acd578864a52
Christopher Chen <christopher.chen.1004@gmail.com>
2023-07-03 21:19:49 -0400

    number buttons work but display extends, trying to fix

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-let firstNum;
-let secondNum;
+let firstNum = ""
+let secondNum = ""
 let operation;
+let getFirstNum = true;
+
+const buttons = document.querySelectorAll('.number, .operator, .single, .double');
+const display = document.querySelector('.display');
+const content = document.querySelector('.display .content');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", function() {
+        switch (button.className) {
+            case "number":
+                if (getFirstNum){
+                    firstNum += button.textContent;
+                    updateDisplay();
+                }
+                else{
+                    secondNum += button.textContent;
+                    updateDisplay();
+                }
+                adjustFontSize();
+        }
+    })
+});
 
 function add(a, b) {
   return a + b;
@@ -33,4 +55,22 @@ function operate(a, b, operator) {
       divide(a, b);
       break;
   }
+}
+
+function updateDisplay(){
+    if (getFirstNum){
+        content.textContent = firstNum;
+    }
+    else{
+        content.textContent = firstNum + " ";
+    }
+}
+
+function adjustFontSize() {
+  
+    // Reduce font size until content fits within the display
+    while (content.offsetWidth > display.offsetWidth) {
+      const fontSize = parseFloat(getComputedStyle(content).fontSize);
+      content.style.fontSize = (fontSize - 1) + 'px';
+    }
 }
